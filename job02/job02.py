e0a1fc7ef1e4286cc878debdc1cb4edd834d86c9
mehdi <mehdi.alouane@laplateforme.io>
2024-01-02 10:46:14 +0100

    Job 1 et 2

---
+++ job02/job02.py
@@ -0,0 +1,9 @@
+class Operation:
+    def __init__(self, nombre1, nombre2):
+        self.nombre1 = nombre1
+        self.nombre2 = nombre2
+        
+p1 = Operation(15, 17)
+
+print ("Le nombre1 est", p1.nombre1)
+print ("Le nombre2 est", p1.nombre2)
