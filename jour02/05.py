e87fa5ca0394d5fc4f631005cbdeaf2c203e54cc
mehdi <mehdi.alouane@laplateforme.io>
2024-01-04 02:05:40 +0100

    Job 5 fini

--- jour02/05.py
+++ jour02/05.py
@@ -1,10 +1,11 @@
 class Voiture:
-    def __init__(self, marque, modele, annee, kilometrage, en_marche=False):
+    def __init__(self, marque, modele, annee, kilometrage, en_marche=False, reservoir=3):
         self.__marque = marque
         self.__modele = modele
         self.__annee = annee
         self.__kilometrage = kilometrage
         self.__en_marche = en_marche
+        self.__reservoir = reservoir
 
     def marque(self, marque):
         self.__marque = marque
@@ -38,12 +39,22 @@ class Voiture:
         return self.__en_marche
 
     def demarrer(self):
-        self.__en_marche = True
-        print("La voiture démarre.")
+        niveau_reservoir = self.__verifier_plein()
+        if niveau_reservoir > 5:
+            self.__en_marche = True
+            print("La voiture démarre.")
+        else:
+            print("Pas assez d'essence")    
+
+
 
     def arreter(self):
         self.__en_marche = False
         print("La voiture est à l'arrêt.")
+        
+    def __verifier_plein(self):
+        return self.__reservoir
+    
 
 ma_voiture = Voiture("Audi", "RS3", 2013, 20000)
 
@@ -51,4 +62,5 @@ ma_voiture = Voiture("Audi", "RS3", 2013, 20000)
 
 ma_voiture.en_marche(True)
 print(ma_voiture.get_marque())
+ma_voiture.demarrer()
 
