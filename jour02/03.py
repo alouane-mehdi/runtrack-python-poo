91e871ac2d52f942c881d06df9de96d1ee0dffc6
mehdi <mehdi.alouane@laplateforme.io>
2024-01-04 01:19:39 +0100

    job3

--- jour02/03.py
+++ jour02/03.py
@@ -3,6 +3,7 @@ class livre:
         self.titre=titre
         self.auteur=auteur
         self.Page=Page
+        self.disponible=True
     def modifTitre(self,titre):
         self.titre=titre
     def modifAuteur(self,auteur):
@@ -14,8 +15,26 @@ class livre:
             print("Erreur : La page doit etre un entier positif.")      
     def afficher(self):
         print(f"Titre : {self.titre} , Auteur: {self.auteur} , Nombre de page : {self.Page}. ")
+        
+        
+    def verification(self):
+        return self.disponible
+    def emprunter(self):
+        if self.verification():         
+            print("Livre emprunté !") 
+            self.disponible == False
+        else:
+            print("Ce livre n'est pas disponible")
+    def rendre(self):
+        if not self.verification():
+            print("Livre rendu !")
+            self.__disponible = True       
+        else:
+            print("Ce livre n'a pas été emprunté")   
             
+             
 l1=livre("Madame Bovary","Gustave Flaubert",468)
 l1.afficher()
 l1.modifPage(10)
 l1.afficher()
+l1.emprunter()
