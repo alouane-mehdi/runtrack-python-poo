bbcce77390ce886d34fce165ed6650159564218d
mehdi <mehdi.alouane@laplateforme.io>
2024-01-04 01:46:20 +0100

    Job 2 a jour

--- jour02/02.py
+++ jour02/02.py
@@ -1,19 +1,19 @@
 class livre:
     def __init__(self,titre,auteur,Page):
-        self.titre=titre
-        self.auteur=auteur
-        self.Page=Page
+        self.__titre=titre
+        self.__auteur=auteur
+        self.__Page=Page
     def modifTitre(self,titre):
         self.titre=titre
     def modifAuteur(self,auteur):
-        self.auteur=auteur
+        self.__auteur=auteur
     def modifPage(self,Page):
         if Page >0 :
-            self.Page=Page
+            self.__Page=Page
         else:
             print("Erreur : La page doit etre un entier positif.")      
     def afficher(self):
-        print(f"Titre : {self.titre} , Auteur: {self.auteur} , Nombre de page : {self.Page}. ")
+        print(f"Titre : {self.__titre} , Auteur: {self.__auteur} , Nombre de page : {self.__Page}. ")
             
 l1=livre("Madame Bovary","Gustave Flaubert",468)
 l1.afficher()
