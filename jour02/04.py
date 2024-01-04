59222c3ce7e88fae59b375e2e309539970ae6b82
mehdi <mehdi.alouane@laplateforme.io>
2024-01-04 01:48:47 +0100

    Job 4 a jour

--- jour02/04.py
+++ jour02/04.py
@@ -1,30 +1,30 @@
 class student:
     def __init__(self,nom,prénom,NumeroEtudiant):
-        self.nom=nom
-        self.prenom=prénom
-        self.NumeroEtudiant=NumeroEtudiant
-        self.Credit=0
-        self.level=self.studentEval()
+        self.__nom=nom
+        self.__prenom=prénom
+        self.__NumeroEtudiant=NumeroEtudiant
+        self.__Credit=0
+        self.__level=self.studentEval()
     def add_credit(self, credit):
         if credit >0:
-            self.Credit += credit
+            self.__Credit += credit
         else :
             print("Erreur : Le montant est négatif")
     def NombreCredit(self):
-        print(f"Le nombre de crédit de {self.prenom} {self.nom} est de {self.Credit} points")
+        print(f"Le nombre de crédit de {self.__prenom} {self.__nom} est de {self.__Credit} points")
     def studentEval(self):
-        if self.Credit >= 90:
+        if self.__Credit >= 90:
             return "Excellent"
-        elif self.Credit >= 80:
+        elif self.__Credit >= 80:
             return "Très bien"
-        elif self.Credit >= 70:
+        elif self.__Credit >= 70:
             return "Bien"
-        elif self.Credit >= 60:
+        elif self.__Credit >= 60:
             return "Passable"
         else:
             return "Insuffisant"
     def studentInfo(self):
-        print(f"Nom : {self.nom} \nPrénom : {self.prenom} \nId : {self.NumeroEtudiant} \nNiveau : {self.studentEval()}")
+        print(f"Nom : {self.__nom} \nPrénom : {self.__prenom} \nId : {self.__NumeroEtudiant} \nNiveau : {self.studentEval()}")
 etudiant1=student("Doe", "John", 145)
 etudiant1.add_credit(73)
 etudiant1.NombreCredit()
